3b745ca49d8ac14980e26520cfed3a23997b305e
tk <594959742@qq.com>
2024-05-15 21:26:05 +0800

    stage

--- cpp/game.cpp
+++ cpp/game.cpp
@@ -17,7 +17,9 @@ enum TileType
 {
 	TileField,
 	TileCity,
-	TileResourceField
+	TileResourceField,
+
+	TileTypeCount
 };
 
 struct Tile
@@ -84,10 +86,51 @@ std::vector<uint> find_path(uint start_id, uint end_id)
 	return ret;
 }
 
+enum BuildingType
+{
+	BuildingTownCenter,
+	BuildingHouse,
+	BuildingBarracks,
+	BuildingTower,
+	BuildingWall,
+
+	BuildingTypeCount
+};
+
+const wchar_t* get_building_name(BuildingType type)
+{
+	switch (type)
+	{
+	case BuildingTownCenter: return L"Town Center";
+	case BuildingHouse: return L"House";
+	case BuildingBarracks: return L"Barracks";
+	case BuildingTower: return L"Tower";
+	case BuildingWall: return L"Wall";
+	}
+	return L"";
+}
+
+struct Building
+{
+	BuildingType type;
+	uint lv;
+};
+
 struct City
 {
 	uint tile_id;
+	std::vector<Building> buildings;
 };
+
+struct TownBuildingSlot
+{
+	vec2 pos;
+	float radius;
+	BuildingType building_type;
+};
+std::vector<TownBuildingSlot> town_building_slots;
+
+int selected_building_slot = -1;
 
 enum ResourceType
 {
@@ -100,14 +143,52 @@ enum ResourceType
 	ResourceTypeCount
 };
 
+const wchar_t* get_resource_field_name(ResourceType type)
+{
+	switch (type)
+	{
+	case ResourceWood: return L"Woodcutter";
+	case ResourceClay: return L"Clay Pit";
+	case ResourceIron: return L"Iron Mine";
+	case ResourceCrop: return L"Crop Field";
+	}
+	return L"";
+}
+
 struct ResourceField
 {
+	uint tile_id;
 	uint lv;
 	ResourceType type;
-	uint value;
+	uint production;
 };
 
-SheetPtr sht_resource_field[ResourceTypeCount] = {};
+struct ResourceFieldData
+{
+	uint cost_wood;
+	uint cost_clay;
+	uint cost_iron;
+	uint cost_crop;
+	uint cost_gold;
+	uint cost_population;
+	uint production;
+
+	void read(Sheet::Row& row, SheetPtr sht)
+	{
+		cost_wood = sht->get_as<uint>(row, "cost_wood"_h);
+		cost_clay = sht->get_as<uint>(row, "cost_clay"_h);
+		cost_iron = sht->get_as<uint>(row, "cost_iron"_h);
+		cost_crop = sht->get_as<uint>(row, "cost_crop"_h);
+		cost_gold = sht->get_as<uint>(row, "cost_gold"_h);
+		cost_population = sht->get_as<uint>(row, "cost_population"_h);
+		production = sht->get_as<uint>(row, "production"_h);
+	}
+};
+
+std::vector<ResourceFieldData> resource_field_datas[ResourceTypeCount];
+
+graphics::ImagePtr img_resources[ResourceTypeCount] = {};
+graphics::ImagePtr img_pop;
 
 struct Lord
 {
@@ -149,46 +230,88 @@ struct Lord
 		return false;
 	}
 
+	int find_resource_field(uint tile_id)
+	{
+		for (auto i = 0; i < resource_fields.size(); i++)
+		{
+			if (resource_fields[i].tile_id == tile_id)
+				return i;
+		}
+		return -1;
+	}
+
 	bool build_resource_field(uint tile_id, ResourceType type)
 	{
 		auto& tile = tiles[tile_id];
 		if (tile.type != TileField)
-			return;
-
-		auto sht = sht_resource_field[type];
-		if (!sht || sht->rows.empty())
-			return false;
-		auto& first_level = sht->rows.front();
-		auto cost_wood = sht->get_as<uint>(first_level, "cost_wood"_h);
-		auto cost_clay = sht->get_as<uint>(first_level, "cost_clay"_h);
-		auto cost_iron = sht->get_as<uint>(first_level, "cost_iron"_h);
-		auto cost_crop = sht->get_as<uint>(first_level, "cost_crop"_h);
-		auto cost_gold = sht->get_as<uint>(first_level, "cost_gold"_h);
-		auto cost_population = sht->get_as<uint>(first_level, "cost_population"_h);
-		if (resources[ResourceWood] < cost_wood ||
-			resources[ResourceClay] < cost_clay ||
-			resources[ResourceIron] < cost_iron ||
-			resources[ResourceCrop] < cost_crop ||
-			resources[ResourceGold] < cost_gold ||
-			provide_population < consume_population + cost_population)
 			return false;
 
-		resources[ResourceWood] -= cost_wood;
-		resources[ResourceClay] -= cost_clay;
-		resources[ResourceIron] -= cost_iron;
-		resources[ResourceCrop] -= cost_crop;
-		resources[ResourceGold] -= cost_gold;
-		consume_population += cost_population;
+		if (resource_field_datas[type].empty())
+			return false;
+		auto& first_level = resource_field_datas[type].front();
+		if (resources[ResourceWood] < first_level.cost_wood ||
+			resources[ResourceClay] < first_level.cost_clay ||
+			resources[ResourceIron] < first_level.cost_iron ||
+			resources[ResourceCrop] < first_level.cost_crop ||
+			resources[ResourceGold] < first_level.cost_gold ||
+			provide_population < consume_population + first_level.cost_population)
+			return false;
+
+		resources[ResourceWood] -= first_level.cost_wood;
+		resources[ResourceClay] -= first_level.cost_clay;
+		resources[ResourceIron] -= first_level.cost_iron;
+		resources[ResourceCrop] -= first_level.cost_crop;
+		resources[ResourceGold] -= first_level.cost_gold;
+		consume_population += first_level.cost_population;
 
 		ResourceField resource_field;
+		resource_field.tile_id = tile_id;
 		resource_field.lv = 1;
 		resource_field.type = type;
-		resource_field.value = sht->get_as<uint>(first_level, "production"_h);
+		resource_field.production = first_level.production;
 		resource_fields.push_back(resource_field);
 
 		tile.type = TileResourceField;
 
 		return true;
+	}
+
+	bool upgrade_resource_field(ResourceField& resource_field)
+	{
+		if (resource_field_datas[resource_field.type].size() <= resource_field.lv)
+			return false;
+
+		auto& next_level = resource_field_datas[resource_field.type][resource_field.lv];
+		if (resources[ResourceWood] < next_level.cost_wood ||
+			resources[ResourceClay] < next_level.cost_clay ||
+			resources[ResourceIron] < next_level.cost_iron ||
+			resources[ResourceCrop] < next_level.cost_crop ||
+			resources[ResourceGold] < next_level.cost_gold ||
+			provide_population < consume_population + next_level.cost_population)
+			return false;
+
+		resources[ResourceWood] -= next_level.cost_wood;
+		resources[ResourceClay] -= next_level.cost_clay;
+		resources[ResourceIron] -= next_level.cost_iron;
+		resources[ResourceCrop] -= next_level.cost_crop;
+		resources[ResourceGold] -= next_level.cost_gold;
+		consume_population += next_level.cost_population;
+
+		resource_field.lv++;
+		resource_field.production = next_level.production;
+
+		return true;
+	}
+
+	uint get_production(ResourceType type)
+	{
+		uint ret = 0;
+		for (auto& field : resource_fields)
+		{
+			if (field.type == type)
+				ret += field.production;
+		}
+		return ret;
 	}
 };
 std::vector<Lord> lords;
@@ -203,6 +326,8 @@ void add_lord(uint tile_id)
 	lord.resources[ResourceIron] = 800;
 	lord.resources[ResourceCrop] = 800;
 	lord.resources[ResourceGold] = 5000;
+	lord.provide_population = 10;
+	lord.consume_population = 0;
 
 	City city;
 	city.tile_id = tile_id;
@@ -260,6 +385,12 @@ void Game::init()
 
 	img_tile_frame = graphics::Image::get(L"assets/HexTilesetv3.png_slices/00.png");
 	img_tile_grass = graphics::Image::get(L"assets/HexTilesetv3.png_slices/01.png");
+	img_resources[ResourceWood] = graphics::Image::get(L"assets/icons/wood.png");
+	img_resources[ResourceClay] = graphics::Image::get(L"assets/icons/clay.png");
+	img_resources[ResourceIron] = graphics::Image::get(L"assets/icons/iron.png");
+	img_resources[ResourceCrop] = graphics::Image::get(L"assets/icons/crop.png");
+	img_resources[ResourceGold] = graphics::Image::get(L"assets/icons/gold.png");
+	img_pop = graphics::Image::get(L"assets/icons/pop.png");
 
 	tiles.resize(tile_cx * tile_cy);
 	for (auto y = 0; y < tile_cy; y++)
@@ -303,10 +434,58 @@ void Game::init()
 		}
 	}
 
-	sht_resource_field[ResourceWood] = Sheet::get(L"assets/woodcutter.sht");
-	sht_resource_field[ResourceClay] = Sheet::get(L"assets/clay_pit.sht");
-	sht_resource_field[ResourceIron] = Sheet::get(L"assets/iron_mine.sht");
-	sht_resource_field[ResourceCrop] = Sheet::get(L"assets/crop_field.sht");
+	if (auto sht = Sheet::get(L"assets/woodcutter.sht"); sht)
+	{
+		for (auto i = 0; i < sht->rows.size(); i++)
+		{
+			ResourceFieldData data;
+			auto& row = sht->rows[i];
+			data.read(row, sht);
+			resource_field_datas[ResourceWood].push_back(data);
+		}
+	}
+	if (auto sht = Sheet::get(L"assets/clay_pit.sht"); sht)
+	{
+		for (auto i = 0; i < sht->rows.size(); i++)
+		{
+			ResourceFieldData data;
+			auto& row = sht->rows[i];
+			data.read(row, sht);
+			resource_field_datas[ResourceClay].push_back(data);
+		}
+	}
+	if (auto sht = Sheet::get(L"assets/iron_mine.sht"); sht)
+	{
+		for (auto i = 0; i < sht->rows.size(); i++)
+		{
+			ResourceFieldData data;
+			auto& row = sht->rows[i];
+			data.read(row, sht);
+			resource_field_datas[ResourceIron].push_back(data);
+		}
+	}
+	if (auto sht = Sheet::get(L"assets/crop_field.sht"); sht)
+	{
+		for (auto i = 0; i < sht->rows.size(); i++)
+		{
+			ResourceFieldData data;
+			auto& row = sht->rows[i];
+			data.read(row, sht);
+			resource_field_datas[ResourceCrop].push_back(data);
+		}
+	}
+	if (auto sht = Sheet::get(L"assets/town_building_slots.sht"); sht)
+	{
+		for (auto i = 0; i < sht->rows.size(); i++)
+		{
+			TownBuildingSlot slot;
+			auto& row = sht->rows[i];
+			slot.pos = sht->get_as<vec2>(row, "pos"_h);
+			slot.radius = sht->get_as<float>(row, "radius"_h);
+			slot.building_type = (BuildingType)sht->get_as<uint>(row, "building"_h);
+			town_building_slots.push_back(slot);
+		}
+	}
 
 	if (auto id = search_lord_location(); id != -1)
 		add_lord(id);
@@ -351,63 +530,190 @@ void Game::on_render()
 	if (main_player)
 	{
 		renderer->hud_begin(vec2(0.f, 0.f), vec2(screen_size.x, 20.f), cvec4(0, 0, 0, 255));
-		renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(8.f, 0.f));
-		renderer->hud_text(std::format(L"Wood: {}", main_player->resources[ResourceWood]), 20);
-		renderer->hud_text(std::format(L"Clay: {}", main_player->resources[ResourceClay]), 20);
-		renderer->hud_text(std::format(L"Iron: {}", main_player->resources[ResourceIron]), 20);
-		renderer->hud_text(std::format(L"Crop: {}", main_player->resources[ResourceCrop]), 20);
-		renderer->hud_text(std::format(L"Gold: {}", main_player->resources[ResourceGold]), 20);
+		renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+		renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceWood]);
+		renderer->hud_text(std::format(L"{} +{}", main_player->resources[ResourceWood], main_player->get_production(ResourceWood)), 16);
+		renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceClay]);
+		renderer->hud_text(std::format(L"{} +{}", main_player->resources[ResourceClay], main_player->get_production(ResourceClay)), 16);
+		renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceIron]);
+		renderer->hud_text(std::format(L"{} +{}", main_player->resources[ResourceIron], main_player->get_production(ResourceIron)), 16);
+		renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceCrop]);
+		renderer->hud_text(std::format(L"{} +{}", main_player->resources[ResourceCrop], main_player->get_production(ResourceCrop)), 16);
+		renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceGold]);
+		renderer->hud_text(std::format(L"{}", main_player->resources[ResourceGold]), 16);
+		renderer->hud_image(vec2(18.f, 12.f), img_pop);
+		renderer->hud_text(std::format(L"{}/{}", main_player->consume_population, main_player->provide_population), 16);
 		renderer->hud_end_layout();
 		renderer->hud_end();
 	}
 
 	renderer->hud_begin(vec2(0.f, screen_size.y - 160.f), vec2(screen_size.x, 160.f), cvec4(0, 0, 0, 255));
+	auto rect = renderer->hud_hud_rect();
 	if (selected_tile != -1)
 	{
 		auto& tile = tiles[selected_tile];
 		switch (tile.type)
 		{
 		case TileField:
-			if (main_player)
+			if (main_player && main_player->has_territory(selected_tile))
 			{
-				if (main_player->has_territory(selected_tile))
+				renderer->hud_begin_layout(HudHorizontal);
+
+				auto show_build_resource_field = [&](ResourceType type) {
+					renderer->hud_begin_layout(HudVertical, vec2(200.f, 150.f));
+					renderer->hud_text(get_resource_field_name(type));
+					if (!resource_field_datas[type].empty())
+					{
+						auto& first_level = resource_field_datas[type].front();
+
+						renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+						renderer->hud_text(L"Production: ", 22);
+						renderer->hud_image(vec2(20.f, 13.f), img_resources[type]);
+						renderer->hud_text(std::format(L"{}", first_level.production), 22);
+						renderer->hud_end_layout();
+
+						renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceWood]);
+						renderer->hud_text(std::format(L"{}", first_level.cost_wood), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceClay]);
+						renderer->hud_text(std::format(L"{}", first_level.cost_clay), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceIron]);
+						renderer->hud_text(std::format(L"{}", first_level.cost_iron), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceCrop]);
+						renderer->hud_text(std::format(L"{}", first_level.cost_crop), 16);
+						renderer->hud_end_layout();
+						renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceGold]);
+						renderer->hud_text(std::format(L"{}", first_level.cost_gold), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_pop);
+						renderer->hud_text(std::format(L"{}", first_level.cost_population), 16);
+						renderer->hud_end_layout();
+
+						if (renderer->hud_button(L"Build", 18))
+							main_player->build_resource_field(selected_tile, type);
+					}
+					renderer->hud_end_layout();
+					renderer->hud_stroke_item();
+				};
+
+				show_build_resource_field(ResourceWood);
+				show_build_resource_field(ResourceClay);
+				show_build_resource_field(ResourceIron);
+				show_build_resource_field(ResourceCrop);
+
+				renderer->hud_end_layout();
+			}
+			break;
+		case TileCity:
+		{
+			auto c = rect.a + vec2(100.f, 80.f);
+			canvas->add_circle_filled(c, 80.f, cvec4(255));
+
+			vec2 tower_pos[6];
+			for (auto i = 0; i < 6; i++)
+				tower_pos[i] = arc_point(c, i * 60.f, 80.f);
+
+			auto& path = canvas->path;
+			for (auto i = 0; i < 6; i++)
+				path.push_back(tower_pos[i]);
+			canvas->stroke(4.f, cvec4(255, 127, 127, 255), true);
+
+			for (auto i = 0; i < 6; i++)
+				canvas->add_circle_filled(tower_pos[i], 8.f, cvec4(255, 80, 80, 255));
+
+			for (auto& slot : town_building_slots)
+				canvas->add_circle_filled(c + slot.pos, slot.radius, cvec4(255, 127, 127, 255));
+
+			if (input->mpressed(Mouse_Left))
+			{
+				for (auto i = 0; i < town_building_slots.size(); i++)
 				{
-					renderer->hud_begin_layout(HudHorizontal);
+					auto& slot = town_building_slots[i];
+					if (distance(mpos, c + slot.pos) < slot.radius)
+					{
+						selected_building_slot = slot.building_type;
+						break;
+					}
+				}
+				if (selected_building_slot == -1)
+				{
+					for (auto i = 0; i < 6; i++)
+					{
+						if (distance(mpos, tower_pos[i]) < 8.f)
+						{
+							selected_building_slot = BuildingTower;
+							break;
+						}
+					}
+				}
+				if (selected_building_slot == -1)
+				{
+					if (auto d = distance(mpos, c); d > 70.f && d < 80.f)
+						selected_building_slot = BuildingWall;
+				}
+			}
+		}
+			break;
+		case TileResourceField:
+			if (auto id = main_player ? main_player->find_resource_field(selected_tile) : -1; id != -1)
+			{
+				auto& resource_field = main_player->resource_fields[id];
+				if (!resource_field_datas[resource_field.type].empty())
+				{
+					renderer->hud_begin_layout(HudVertical, vec2(200.f, 150.f));
+					renderer->hud_text(get_resource_field_name(resource_field.type));
+					renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+					renderer->hud_text(L"Current Production: ", 22);
+					renderer->hud_image(vec2(20.f, 13.f), img_resources[resource_field.type]);
+					renderer->hud_text(std::format(L"{}", resource_field.production), 22);
+					renderer->hud_end_layout();
+					if (resource_field.lv < resource_field_datas[resource_field.type].size())
+					{
+						auto& next_level = resource_field_datas[resource_field.type][resource_field.lv];
 
-					renderer->hud_begin_layout(HudVertical, vec2(150.f));
-					renderer->hud_text(L"Woodcutter");
-					if (renderer->hud_button(L"Build", 18))
-						;
+						renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+						renderer->hud_text(std::format(L"Level {} Production: ", resource_field.lv + 1), 22);
+						renderer->hud_image(vec2(20.f, 13.f), img_resources[resource_field.type]);
+						renderer->hud_text(std::format(L"{}", next_level.production), 22);
+						renderer->hud_end_layout();
+
+						renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceWood]);
+						renderer->hud_text(std::format(L"{}", next_level.cost_wood), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceClay]);
+						renderer->hud_text(std::format(L"{}", next_level.cost_clay), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceIron]);
+						renderer->hud_text(std::format(L"{}", next_level.cost_iron), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceCrop]);
+						renderer->hud_text(std::format(L"{}", next_level.cost_crop), 16);
+						renderer->hud_end_layout();
+						renderer->hud_begin_layout(HudHorizontal, vec2(0.f), vec2(3.f, 0.f));
+						renderer->hud_image(vec2(18.f, 12.f), img_resources[ResourceGold]);
+						renderer->hud_text(std::format(L"{}", next_level.cost_gold), 16);
+						renderer->hud_image(vec2(18.f, 12.f), img_pop);
+						renderer->hud_text(std::format(L"{}", next_level.cost_population), 16);
+						renderer->hud_end_layout();
+
+						if (renderer->hud_button(L"Upgrade", 18))
+							main_player->upgrade_resource_field(resource_field);
+					}
 					renderer->hud_end_layout();
 					renderer->hud_stroke_item();
-
-					renderer->hud_begin_layout(HudVertical, vec2(150.f));
-					renderer->hud_text(L"Clay Pit");
-					if (renderer->hud_button(L"Build", 18))
-						;
-					renderer->hud_end_layout();
-					renderer->hud_stroke_item();
-
-					renderer->hud_begin_layout(HudVertical, vec2(150.f));
-					renderer->hud_text(L"Iron Mine");
-					if (renderer->hud_button(L"Build", 18))
-						;
-					renderer->hud_end_layout();
-					renderer->hud_stroke_item();
-
-					renderer->hud_begin_layout(HudVertical, vec2(150.f));
-					renderer->hud_text(L"Crop Field");
-					if (renderer->hud_button(L"Build", 18))
-						;
-					renderer->hud_end_layout();
-					renderer->hud_stroke_item();
-
-					renderer->hud_end_layout();
 				}
 			}
 			break;
-		case TileResourceField:
-			break;
+		}
+	}	
+	for (auto& lord : lords)
+	{
+		for (auto& city : lord.cities)
+		{
+
+		}
+		for (auto& field : lord.resource_fields)
+		{
+			auto pos = tiles[field.tile_id].pos + vec2(tile_sz) * 0.5f - vec2(18.f, 12.f);
+			canvas->add_image(img_resources[field.type]->get_view(), pos, pos + vec2(36.f, 24.f), vec4(0.f, 0.f, 1.f, 1.f), cvec4(255));
 		}
 	}
 	renderer->hud_end();
